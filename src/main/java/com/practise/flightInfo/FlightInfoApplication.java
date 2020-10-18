53dc5119b58807af5ff4e799a13bc2acd12bfdc7
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2020-10-18 15:30:53 +0200

    First commit. Project set up. mnvw included

---
+++ src/main/java/com/practise/flightInfo/FlightInfoApplication.java
@@ -0,0 +1,13 @@
+package com.practise.flightInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practise/flightInfo/FlightInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.flightInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
