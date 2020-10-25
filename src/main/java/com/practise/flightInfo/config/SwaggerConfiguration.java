9a4540d5af75cc23b7ceead59573ad7b3922f32b
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2020-10-25 20:32:05 +0100

    changed some info for swagger

--- src/main/java/com/practise/flightInfo/config/SwaggerConfiguration.java
+++ src/main/java/com/practise/flightInfo/config/SwaggerConfiguration.java
@@ -5,14 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @EnableSwagger2
 @Configuration
@@ -22,7 +21,7 @@ public class SwaggerConfiguration extends WebMvcConfigurationSupport {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage(getClass().getPackage().getName())))
                 .build()
                 .apiInfo(apiInfo());
     }
@@ -35,9 +34,11 @@ public class SwaggerConfiguration extends WebMvcConfigurationSupport {
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo("My REST API Flight Information Finder Cache",
-                "This microservice has the responsability of obtaining information of a flight from its own NoSql memory or from an external service and save it",
-                "v1", "urn:tos", new Contact("Emiliano Castellanos", "https://www.linkedin.com/in/emiliano-castellanos", "castellanos.emiliano@gmail.com"),
-                "no license", "no license api", Collections.EMPTY_LIST);
+        return new ApiInfoBuilder()
+                .title("My REST API Flight Information Finder Cache")
+                .description("This microservice has the responsability of obtaining information of a flight from its own NoSql memory or from an external service and save it")
+                .version("v1")
+                .contact(new Contact("Emiliano Castellanos", "https://www.linkedin.com/in/emiliano-castellanos", "castellanos.emiliano@gmail.com"))
+                .build();
     }
 }
